ff48f096dc586f9419f909b37e39424557d95163
User <Willspri@gmail.com>
2024-03-21 13:05:20 +1300

    not found error fixed

--- timeConvert/timeConvert.py
+++ timeConvert/timeConvert.py
@@ -1,16 +1,21 @@
 from datetime import datetime
 import pytz 
 
+#function that will retrieve correct country timezone
 def TimeToCountry(currentTime, country):
-        try:
-            countryTimeZone = pytz.timezone(pytz.country_timezones[country[0]])
-        except KeyError:
-            return "Country not found"
-        try:
-            countryTime = currentTime.astimezone(countryTimeZone)
-            return countryTime.strftime("%Y-%m-%d %H:%M:%S %z")
-        except Exception as e:
-            return f"Error converting time: {e}" 
+    try:
+        countryTimezones = pytz.country_timezones[country]
+        if countryTimezones:
+            countryTimeZone = pytz.timezone(countryTimezones[0]) 
+        else:
+            return "No timezone found for this country code"
+    except KeyError:
+        return "Country not found"
+    try:
+        countryTime = currentTime.astimezone(countryTimeZone)
+        return countryTime.strftime("%Y-%m-%d %H:%M:%S %z")
+    except Exception as e:
+        return f"Error converting time: {e}" 
         
 if __name__ == "__main__":
      currentTime = datetime.now(pytz.utc) 
